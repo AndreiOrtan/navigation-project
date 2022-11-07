4a85ea55476badf24d50476acbbca308514e8176
AndreiOrtan <92335099+AndreiOrtan@users.noreply.github.com>
2022-11-07 13:02:11 +0200

    Translate (#3)
    * accordion
    
    * fixed
    
    * translate
    
    * translate and update dropdown

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import SearchBar from "./components/SearchBar";
+import Translate from "./components/Translate";
 
 const questions = [
   {
@@ -39,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <Dropdown options={options} />
+      <Translate />
     </div>
   );
 };
